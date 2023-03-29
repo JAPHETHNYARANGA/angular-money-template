b94bc0c54e518982803aaa9efcbdd1517890a01f
JAPHETHNYARANGA <nyaranga4@gmail.com>
2023-03-29 09:53:37 +0300

    finished the navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public showNavItems = false;
+  public isMobileView = false;
+
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event: any){
+  //   this.isMobileView = event.target.innerWidth <= 770;
+  //   if(!this.isMobileView){
+  //     this.showNavItems = true;
+  //   }
+  // }
+
+  // toggleNav(){
+  //   this.showNavItems = !this.showNavItems;
+  // }
 
 }
